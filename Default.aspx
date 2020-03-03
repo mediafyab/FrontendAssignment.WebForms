68523e0d1f6b10ace14f428fa6c04b7610e60c02
Robert Fricke <fricke@mediafy.com>
2020-03-03 10:31:02 +0100

    Initail Commit

---
+++ Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Products Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FrontendAssignment._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <table>
+        <asp:Repeater ID="rpt" runat="server">
+            <ItemTemplate>
+                <tr>
+                    <td>
+                        <img src="<%# Eval("Image")%>" width="40" />
+                    </td>
+                    <td><%# Eval("Name")%></td>
+                    <td><%# Eval("FromPrice")%></td>
+                </tr>
+            </ItemTemplate>
+        </asp:Repeater>
+    </table>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web;
+using Newtonsoft.Json;
+using System.Web.UI;
+using System.IO;
+
+namespace FrontendAssignment.WebForms
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Product[] products = null;
+            var filePath = HttpContext.Current.Server.MapPath("~/App_Data/Products.json");
+            using (var reader = new StreamReader(filePath))
+            {
+                products = JsonConvert.DeserializeObject<Product[]>(reader.ReadToEnd());
+            }
+
+            rpt.DataSource = products;
+            rpt.DataBind();
+        }
+
+        public class Product
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("fromPrice")]
+            public string FromPrice { get; set; }
+
+            [JsonProperty("image")]
+            public string Image { get; set; }
+
+            [JsonProperty("description")]
+            public string Description { get; set; }
+        }
+    }
+}
